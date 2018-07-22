1b848db2b022e2ee851fd3f9c28874d8aad055e8
Kiran PS <pskirann@gmail.com>
2018-07-22 21:06:25 +0530

    added vscode settings and prettier on save

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
-import Login from 'pages/Login'
-import {
-  BrowserRouter as Router,
-  Route,
-} from 'react-router-dom'
-
+import React, { Component } from "react";
+import Login from "pages/Login";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route exact path="/login" component={Login}/>
+        <Route exact path="/login" component={Login} />
       </Router>
     );
   }
